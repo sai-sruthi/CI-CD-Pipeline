ecd71d3a834718f90c6baa082c74e743a43faaf0
Sakthi <srmuruga@ncsu.edu>
2021-04-28 23:19:24 -0400

    Cloning repo into the vm based on the passed branch

--- commands/canary.js
+++ commands/canary.js
@@ -4,8 +4,10 @@ const os    = require('os');
 const path  = require('path');
 const bakerx = require('../lib/bakerx');
 const child = require('child_process');
-
-
+const ssh = require('../lib/ssh');
+const configuration = require('../local-env.json');
+const blue = configuration.blue;
+const green = configuration.green;
 
 exports.command = 'canary [brancha] [branchb]';
 exports.desc = 'Provision a new local development environment and perform canary analysis';
@@ -36,7 +38,8 @@ exports.handler = async argv => {
 async function run(brancha, branchb) {
 
     await provision();
-
+    await initiateApp(brancha, blue);
+    await initiateApp(branchb, green);
 }
 
 async function provision(){
@@ -54,3 +57,20 @@ async function provision(){
     await bakerx.execute("run",``).catch(e => e);
 
 }
+
+async function initiateApp(branch, site) {
+
+    host = `${site.user}@${site.ip}`;
+    console.log(chalk.greenBright('Install App and initiate node!'));
+    
+    //Kill stray node processes
+    await ssh(`sudo killall node`, host);
+
+    //clean and clone
+    await ssh(`sudo rm -r ~/checkpoint.io/www; sudo mkdir ~/checkpoint.io/www; cd ~/checkpoint.io/www; sudo git clone -b ${branch} https://github.com/chrisparnin/checkbox.io-micro-preview.git`, host);
+    
+    //npm install and pm2 start
+    await ssh(`cd ~/checkpoint.io/www/checkbox.io-micro-preview;sudo npm install; sudo pm2 start index.js;`, host);
+
+}
+
